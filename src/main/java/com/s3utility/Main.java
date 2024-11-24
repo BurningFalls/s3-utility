b0278daefbb4ca8253dd275fa7eb47ecf1bf2356
BurningFalls <sherlocky31415@gmail.com>
2024-11-24 14:02:48 +0900

    feat: region도 env로 관리

--- src/main/java/com/s3utility/Main.java
+++ src/main/java/com/s3utility/Main.java
@@ -4,32 +4,32 @@ import io.github.cdimascio.dotenv.Dotenv;
 import software.amazon.awssdk.regions.Region;
 
 public class Main {
-    private static final Region REGION = Region.AP_NORTHEAST_2;
 
     public static void main(String[] args) {
         Dotenv dotenv = Dotenv.load();
         String accessKey = dotenv.get("AWS_ACCESS_KEY");
         String secretKey = dotenv.get("AWS_SECRET_ACCESS_KEY");
+        Region region = Region.of(dotenv.get("AWS_REGION"));
         String sourceBucket = dotenv.get("AWS_SOURCE_BUCKET");
         String sourceFolder = dotenv.get("AWS_SOURCE_FOLDER");
         String targetBucket = dotenv.get("AWS_TARGET_BUCKET");
         String targetFolder = dotenv.get("AWS_TARGET_FOLDER");
 
-        s3FileCopy(accessKey, secretKey, sourceBucket, sourceFolder, targetBucket, targetFolder);
-//        s3FileDownload(accessKey, secretKey, sourceBucket, sourceFolder);
+        s3FileCopy(accessKey, secretKey, region, sourceBucket, sourceFolder, targetBucket, targetFolder);
+//        s3FileDownload(accessKey, secretKey, region, sourceBucket, sourceFolder);
     }
 
-    private static void s3FileCopy(String accessKey, String secretKey,
+    private static void s3FileCopy(String accessKey, String secretKey, Region region,
                                    String sourceBucket, String sourceFolder, String targetBucket, String targetFolder) {
-        S3FileCopier copier = new S3FileCopier(accessKey, secretKey, REGION,
+        S3FileCopier copier = new S3FileCopier(accessKey, secretKey, region,
                 sourceBucket, sourceFolder, targetBucket, targetFolder);
 
         copier.copyFolderBetweenBuckets();
     }
 
-    private static void s3FileDownload(String accessKey, String secretKey,
+    private static void s3FileDownload(String accessKey, String secretKey, Region region,
                                        String sourceBucket, String sourceFolder) {
-        S3FileDownloader downloader = new S3FileDownloader(accessKey, secretKey, REGION, sourceBucket, sourceFolder);
+        S3FileDownloader downloader = new S3FileDownloader(accessKey, secretKey, region, sourceBucket, sourceFolder);
 
         downloader.downloadFilesWithPrefix();
     }
